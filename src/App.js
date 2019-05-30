cde5169015d61b1cafd58de7f573234ed5c83f17
addil <aadil01@hotmail.co.uk>
2019-05-30 21:37:45 +0100

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
 import './App.css';
+import {Container, Header, Menu} from "semantic-ui-react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Fragment>
+            <Menu inverted>
+                <Container>
+                {/*<Menu.Item header><span style={{color: '#da252f', marginRight: 10}}>ENGINE </span> | <span*/}
+                {/*    style={{marginLeft: 10}}>Transformation</span></Menu.Item>           */}
+                    <Menu.Item header>ENGINE </Menu.Item>
+                </Container>
+            </Menu>
+            <Container text>
+                <Header as='h2'>Live departures at Great Portland Street</Header>
+            </Container>
+        </Fragment>
+    );
 }
 
 export default App;

7ea484a0e699c7749a8edd8d477d8ce8dc7cb7b3
addil <aadil01@hotmail.co.uk>
2019-06-02 14:27:57 +0100

    Status colour

--- src/components/LineDetails.js
+++ src/components/LineDetails.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import LineLabel from "./LineLabel";
+import {Label} from "semantic-ui-react";
 
 
 function LineDetails(props){
     const {line} = props;
     const status = line.lineStatuses[0].statusSeverityDescription;
+    let statusColour = null;
 
+    switch (status) {
+        case 'Good Service':
+            statusColour = 'green';
+            break;
+        default:
+            statusColour = 'orange';
+    }
+    
     return (
-         <div style={{display: 'inline', paddingRight: 20}}>
-             <LineLabel id={line.id} name={line.name}/> {status}
+         <div style={{display: 'inline', paddingRight: 30, verticalAlign: 'middle'}}>
+             <LineLabel id={line.id} name={line.name}/>
+             <Label circular color={statusColour} empty style={{marginRight: 5, verticalAlign: 'middle'}}/>
+             <span style={{verticalAlign: 'middle'}}>{status}</span>
          </div>
     )
 }
